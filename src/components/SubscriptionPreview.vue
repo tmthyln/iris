c0cf975dad7ba1af1f2091e5c45a11562a496421
Tim Lin <32549553+tmthyln@users.noreply.github.com>
2026-01-28 11:47:27 -0500

    chore: remove unused import

--- src/components/SubscriptionPreview.vue
+++ src/components/SubscriptionPreview.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {computed, toRef} from 'vue';
+import {toRef} from 'vue';
 import {Feed} from "../types.ts";
 import {useUnescapedHTML} from "../htmlproc.ts";
 import {usePlaceholderImage} from "../placeholderImage.ts";
